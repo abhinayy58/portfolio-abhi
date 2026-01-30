eb8d17029d7835d928a789269d1a5519ec2c1b44
Abhinay yadav <abhinayy48@gmail.com>
2026-01-30 20:28:24 +0530

    Fix layout issues in Hero component and correct motion element tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col  justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -74,7 +74,7 @@ const Hero = () => {
            rounded-3x1 border-4 border-secondary 
           flex justify-center items-start p-2"
           >
-            <motion.dev
+            <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
