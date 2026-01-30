a9dfc2245000a8bff6195bed4146cca8fabeb8ba
Abhinay yadav <abhinayy48@gmail.com>
2026-01-30 20:29:27 +0530

    Remove unused variable warnings in Works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { Tilt } from "react-tilt"
 import {motion} from "framer-motion"
 import {styles} from '../styles'
